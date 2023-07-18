7c344bdbde2a4af932de5884990b7bf0ea434ca3
everton-EVAM <everton_menezes_@outlook.com>
2023-07-18 23:02:40 +0000

    feat: adicionando mode do jogador

--- src/apps/players/models.py
+++ src/apps/players/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class PLayers(models.Model):
+
+    name = models.CharField("Nome", max_length=100, null=False, blank=True)
+    highest_ranking = models.IntegerField('Ranking mais alto', null=True, blank=True)
+    current_ranking = models.IntegerField("Ranking atual", null=True, blank=True)
+    years = models.IntegerField('Idade', null=False, blank=True)
+    favorite_surface = models.CharField('Superfície favorita', max_length=20, null=True, blank=False) 
--- src/apps/sport/models.py
+++ src/apps/sport/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+class Sport(models.Model):
+
+    UNDEFINED = 0
+    FUTEBOL = 1
+    BASKETBALL = 2
+    TENNIS = 3
+    FORMULA_ONE = 4
+    MMA = 5
+    SURF = 6
+    CYCLING = 7
+
+    SPORT_CHOICES = (
+        (UNDEFINED, "Indefinido")
+        (FUTEBOL, "Futebol"),
+        (BASKETBALL, "Basquete"),
+        (TENNIS, "Tênis"),
+        (FORMULA_ONE, "Formula 1"),
+        (MMA, "MMA"),
+        (SURF, "Surf"),
+        (CYCLING, "Ciclismo"),
+    )
+
+    sport = models.IntegerField('Esport', null=False, blank=True, default=UNDEFINED)
+
+
+class Games(models.Model):
+
+    game_date = models.DateField('Data do jogo', null=False, blank=True)
+    participants = models.CharField('Participantes', max_length=255, null=False, blank=True)
+
+
+class League(models.Model):
+
+    name = models.CharField("Nome", max_length=100, null=False, blank=True)
+    participants = models.CharField('Participantes', max_length=255, null=False, blank=True)
+    maximum_participants = models.IntegerField('maximo de participantes', null=True, blank=False)
